119b6f747093a65b20b2756cf873eecd1e4baf8e
Your Name <you@example.com>
2023-07-10 14:03:40 +0700

    Configurations
    - prisma
    - update gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaClientModule } from './packages/prisma-client/prisma-client.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
