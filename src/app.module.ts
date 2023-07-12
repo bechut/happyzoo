08c7d63fdeed1d0b3e718f886c8494624e05e4a7
Your Name <you@example.com>
2023-07-12 11:59:55 +0700

    Config #2
    - add interceptors (response, exception)
    - add lodash
    - lintting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaClientModule } from './packages/prisma-client/prisma-client.module';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { ResponseInterceptor } from './packages/interceptors/response.interceptor';
+import { ExceptionInterceptor } from './packages/interceptors/exception.interceptor';
 
 @Module({
   imports: [PrismaClientModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionInterceptor,
+    },
+  ],
 })
 export class AppModule {}
