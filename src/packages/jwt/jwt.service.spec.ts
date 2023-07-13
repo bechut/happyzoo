4557e4d3954187917dfad430745a13b9b09992fc
Your Name <you@example.com>
2023-07-13 17:12:46 +0700

    Config#3 add Jwt & update unit test

---
+++ src/packages/jwt/jwt.service.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from './jwt.service';
+import { JwtModule } from '@nestjs/jwt';
+
+describe('JwtService', () => {
+  let service: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
+      providers: [JwtService],
+    }).compile();
+
+    service = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('get() toBeDefined', () => {
+    expect(service.get).toBeDefined();
+    expect(typeof service.get()).toBe('object');
+  });
+});
