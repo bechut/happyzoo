2d2f9a34ffc53cc50a37388828074b0e313cc2bc
Your Name <you@example.com>
2023-07-17 17:27:19 +0700

    feature create user

---
+++ src/unit_test/mock-user-client.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from './../user/user.dto';
+import { mockUser } from './mock-data';
+import { BadRequestException } from '@nestjs/common';
+
+export const mockUserClientCreate = (payload: CreateUserDto) => {
+  if (payload.email === mockUser.email)
+    throw new BadRequestException('User exists');
+  return payload;
+};
