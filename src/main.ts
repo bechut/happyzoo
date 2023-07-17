f1caf79b6b4b892767f48e9eaa9d0d296a47002d
Your Name <you@example.com>
2023-07-17 16:46:50 +0700

    Feature create user
    - update unit test
    - update migration
    - update config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
   await app.listen(port);
-  asdasd1;
 }
 bootstrap();
